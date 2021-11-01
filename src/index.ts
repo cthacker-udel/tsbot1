aab784fbe1891755f6d0cb0b2a1a858dbbb1d985
Cameron Thacker <cthacker@udel.edu>
2021-11-01 05:08:05 -0400

    Added database connection

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 
 import { configtype } from "../interfaces/configtype";
-import { Intents, Client, Message } from 'discord.js';
-import { token, guildId, clientId } from "../config.json";
+import { Intents, Client, Message, MessageReaction, User } from 'discord.js';
+import { token, guildId, clientId, connectString } from "../config.json";
+import expressions from "../expressions.json";
+import { MongoClient } from "mongodb";
 
 const commands = [{
     name: 'ping',
@@ -18,15 +20,48 @@ const client = new Client({
               Intents.FLAGS.GUILD_MESSAGE_REACTIONS]
 });
 
+const mongoClient = new MongoClient(connectString);
+
+mongoClient.connect(err => {
+    const collection = mongoClient.db("Sage").collection("admins");
+    //console.log(collection);
+    collection.insertOne({ user: "dalizardking", perms: "admin"}, (err, res) => {
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log("no errors");
+            mongoClient.close();
+        }
+    });
+});
 
 client.once('ready', () => {
     console.log('Ready');
+
 });
+
 
 client.on('messageCreate', (msg: Message) => {
 
     console.log(msg);
-    console.log(msg.channel);
+    if(msg.content.startsWith('bot;')){
+
+        if(new RegExp(expressions.UserMessages.date).test(msg.content)){
+            msg.channel.send(new Date().toISOString());
+        }
+        else{
+            msg.channel.send('Command activated');
+        }
+    }
+
+});
+
+client.on('messageReactionAdd', (msgReact: MessageReaction, user: User) => {
+
+    console.log(msgReact);
+
+    console.log(user);
 
 });
 
